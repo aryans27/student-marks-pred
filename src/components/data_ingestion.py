da56c6b2c1eabd3312d5459178bb1bf732ec1afa
Sakshit <sakshitaryan@gmail.com>
2025-04-01 16:12:34 +0530

    5th Commit "model trainer"

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,7 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass       # used to create class variables
 
 from src.components.data_transformation import DataTransformation, DataTransformationConfig
+from src.components.model_training import ModelTrainerConfig, ModelTrainer
 
 @dataclass
 class DataIngestionCofig:
@@ -52,3 +53,6 @@ if __name__=='__main__':
 
     data_transformation=DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
